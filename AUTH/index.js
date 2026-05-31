2330f7cad15d737efd407c732ad47db2ac79d09e
krishna singh <0.krishna1120@gmail.com>
2026-05-31 11:46:23 +0530

    Implement notes API with POST and GET endpoints

--- AUTH/index.js
+++ AUTH/index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 
 const app = express();
+const notes = [];
+
+app.post("/notes", (req, res) => {
+    const note = req.body.note;
+    notes.push(note);
+    res.json({ message: "Note added successfully" });
+});
+
+app.get("/notes", (req, res) => {
+    res.json({ notes });
+});
 
 app.listen(3000);
