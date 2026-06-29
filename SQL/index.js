2e43705f9fcaa787197c01991db7eacbe60a927a
krishna singh <0.krishna1120@gmail.com>
2026-06-29 08:48:28 +0530

    chore: add PostgreSQL dependency and setup database connection in index.js

--- SQL/index.js
+++ SQL/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
-
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: 'postgresql://neondb_owner:npg_Pdko5yKBw8fH@ep-lucky-voice-atf6na7j-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+})
 const app = express();
 app.use(express.json());
 
-app.post
+app.post("/signup",(req,res)=>{
+    
+})
+app.post("/signin",(req,res)=>{
+
+})
+app.listen(3000);
