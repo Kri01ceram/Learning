d5c1a033706c54a3a5c0f528c71a28d182a40cc2
krishna singh <0.krishna1120@gmail.com>
2026-08-03 16:07:37 +0530

    style: update layout of Auth screen with flexbox and add placeholder text

--- REACT/trello-frn/vite-project/src/screens/Auth.tsx
+++ REACT/trello-frn/vite-project/src/screens/Auth.tsx
@@ -3,9 +3,11 @@ import  AuthCredentials  from '../componenets/AuthCredentials'
 export function
 Auth() {
   return (
-    <div>
-      <AuthBanner />
-      <AuthCredentials />
+    <div style = {{display: 'flex'}}>
+      <div style = {{flex: 1}}><AuthBanner /> </div>
+      <div style = {{flex: 1}}><AuthCredentials /></div>
+      
+      
     </div>
   )
 }
